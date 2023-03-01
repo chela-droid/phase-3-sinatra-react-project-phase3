2e9fe87d7d8beff4b9199d27a24fc146dcd973af
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-03-01 14:29:22 +0300

    data generationn

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_104007) do
-
-  create_table "inventories", force: :cascade do |t|
-    t.string "name"
-    t.integer "quantity"
-    t.datetime "expiration_date"
-  end
-
-  create_table "patients", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "address"
-    t.integer "inventory_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+20.times do
+    Inventory.create(
+      name: Faker::Name.unique.name,
+      quantity: Faker::Number.between(from: 1, to: 100),
+      expiration_date: Faker::Date.between(from: Date.today, to: 1.year.from_now)
+    )
+  end
+  
 
 puts "✅ Done seeding!"
