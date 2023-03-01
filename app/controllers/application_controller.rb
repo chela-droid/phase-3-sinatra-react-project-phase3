6a99a262995e232ce1c575e83febd37c09720cec
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-03-02 02:28:40 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/inventory" do
+    # { message: "caroline!" }.to_json
+  inventory = Inventory.all
+  p inventory
+  inventory.to_json()
+  end
+
+  get "/inventory/:pk" do
+  inventory = Inventory.find_by(id: params[:pk])
+  inventory.to_json()
+  end
+  # posting
+  post "/inventory/" do
+  inventory = Inventory.create(
+    name:params[:name],
+    quantity:params[:quantity],
+    expiration_date:params[:expiration_date],
+  )
+  inventory.save()
+
+  {
+    "Message": "created successfully",
+    "Status": "HTTP_200_OK"
+  }.to_json()
+
   end
 
 end
